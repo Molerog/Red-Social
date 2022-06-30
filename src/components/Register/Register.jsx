93b7c452ff74a905a38e849912a8f879b09f68c8
Molerog <102958319+Molerog@users.noreply.github.com>
2022-06-30 17:46:58 +0200

    añadidas validaciones al registro

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,82 +1,121 @@
 import { Form, Input, Button, notification, Space } from "antd";
-import {useDispatch} from 'react-redux';
-import {register} from '../../features/auth/authSlice';
-import {useNavigate} from "react-router-dom";
-
-
+import { useDispatch } from "react-redux";
+import { register } from "../../features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-
-    const openNotificationWithIcon = (type) => {
-        notification[type]({
-          message: 'Registro realizado con éxito!',
-          description:
-            'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
-        });
-      };
-    const navigate = useNavigate();   
-    const dispatch = useDispatch()
-    const onFinish = (values)=>{
-             dispatch(register(values))
-             openNotificationWithIcon('success')
-             setTimeout(() => {
-                navigate('/login');
-             }, 3000);
-    }
-    const onFinishFailed = (errorInfo) => {
-        console.log("Failed:", errorInfo);
-    }
+  const openNotificationWithIcon = (type) => {
+    notification[type]({
+      message: "Registro realizado con éxito!",
+      description:
+        "This is the content of the notification. This is the content of the notification. This is the content of the notification.",
+    });
+  };
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const onFinish = (values) => {
+    console.log(values)
+    dispatch(register(values));
+    openNotificationWithIcon("success");
+    setTimeout(() => {
+      navigate("/login");
+    }, 3000);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
     <div className="container">
-            <Form
-                name="basic"
-                labelCol={{ span: 8 }}
-                wrapperCol={{ span: 16 }}
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete="off"
-            >
-                <Form.Item
-                    label="Name"
-                    name="name"
-                    rules={[{ required: true, message: "Please input your name!" }]}
-                >
-                    <Input />
-                </Form.Item>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Name"
+          name="name"
+          rules={[{ required: true, message: "Please input your name!" }]}
+        >
+          <Input />
+        </Form.Item>
 
-                <Form.Item
-                    label="Email"
-                    name="email"
-                    rules={[{ required: true, message: "Please input your email!" }]}
-                >
-                    <Input />
-                </Form.Item>
+        <Form.Item
+          name="email"
+          label="Email"
+          rules={[
+            {
+              type: "email",
+              message: "The input is not valid E-mail!",
+            },
+            {
+              required: true,
+              message: "Please input your E-mail!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[{ required: true, message: "Please input your password!" }]}
-                >
-                    <Input.Password />
-                </Form.Item>
+        <Form.Item
+          name="password"
+          label="Password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password />
+        </Form.Item>
 
-                {/* <div className='textContainer'>
+        <Form.Item
+          name="confirm"
+          label="Confirm Password"
+          dependencies={["password"]}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: "Please confirm your password!",
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+
+                return Promise.reject(
+                  new Error("The two passwords that you entered do not match!")
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        {/* <div className='textContainer'>
                     <span>¿Ya estás registrado?</span> <Link to='/login'>Inicia sesión</Link>
                 </div>
 
                 {message} */}
 
-                <br/>
+        <br />
 
-                <Form.Item wrapperCol={{ offset: 9, span: 16 }}>
-                    <Button type="primary" htmlType="submit">
-                        Enviar
-                    </Button>
-                </Form.Item>
-            </Form>
-        </div>
-  )
-}
+        <Form.Item wrapperCol={{ offset: 9, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Enviar
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
