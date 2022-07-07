d4179c1c40b9cd36471333705b3d69c060b5fb34
Molerog <102958319+Molerog@users.noreply.github.com>
2022-07-07 17:02:38 +0200

    arreglando warnings

--- src/components/Profile/UserLogged/UserLogged.jsx
+++ src/components/Profile/UserLogged/UserLogged.jsx
@@ -1,26 +1,22 @@
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 
-import InfoUserLogged from '../UserLogged/InfoUserLogged.jsx/InfoUserLogged'
-import { getInfo } from '../../../features/posts/postsSlice';
+import InfoUserLogged from "../UserLogged/InfoUserLogged.jsx/InfoUserLogged";
+import { getInfo } from "../../../features/posts/postsSlice";
 
 const UserLogged = () => {
-    const {posts} = useSelector((state => state.posts))
-    const dispatch = useDispatch()  
-    const infoUser = async() => {  
-        await dispatch(getInfo());
-      };
+  const { posts } = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
 
-      useEffect(() => {
-        dispatch(getInfo());
-      }, [posts]);
-    
+  useEffect(() => {
+    dispatch(getInfo());
+  }, [posts]);
+
   return (
     <>
-    <div>{infoUser}</div>
-    <InfoUserLogged />
+      <InfoUserLogged />
     </>
-  )
-}
+  );
+};
 
-export default UserLogged
+export default UserLogged;
