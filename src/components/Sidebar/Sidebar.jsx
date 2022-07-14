6ae93f011b2f68648630855e8e9d570fbf00e08a
Molerog <102958319+Molerog@users.noreply.github.com>
2022-07-14 02:37:00 +0200

    cambios menores

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,8 +5,8 @@ const Sidebar = () => {
   return (
     <div className="RightBar">
       <h4 className="RightBarTitle">Usuarios Registrados</h4>
-      <div className='Scroll'>
-      <UserRegistered />
+      <div className="Scroll">
+        <UserRegistered />
       </div>
     </div>
   );
